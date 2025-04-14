417dbf04ba5a1c93a4eef8ca8bc70ed26a8de094
Viswanathan Seetharaman <links96.4u@outlook.com>
2025-04-14 21:31:32 +0530

    Added the methods for selecting preferences.

--- browser/browser.py
+++ browser/browser.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from config.config import SEE_All_LINK_XPATH
+from config.config import SEE_All_LINK_XPATH ,PREFERENCE_CHECK_BOX
 import time
 
 def setup_driver():
@@ -44,12 +44,27 @@ def navigate_to_inbox(driver):
     sent_link.click()
     time.sleep(5)
 
-def navigate_to_matches(driver):
+def navigate_to_more_matches(driver):
     # Navigate to Matches for Me
     matches_link = driver.find_element(By.LINK_TEXT, 'More Matches')
     matches_link.click()
     time.sleep(5)
-    see_all_button = driver.find_element(By.XPATH,SEE_All_LINK_XPATH)
+
+def navigate_to_see_all(driver,type = 1):
+    LINK_XPATH = ""
+    if type == 1:
+        LINK_XPATH =SEE_All_LINK_XPATH
+    see_all_button = driver.find_element(By.XPATH,LINK_XPATH)
     see_all_button.click()
     time.sleep(5)
     driver.switch_to.window(driver.window_handles[1])
+    time.sleep(2)
+
+def check_options_list(driver):
+    for preference in PREFERENCE_CHECK_BOX:
+        checkbox = driver.find_element(By.ID, preference)  # Select the preference one by one
+        checkbox.click()
+        time.sleep(5)
+
+
+    
