45fce8f272c1f05a206fba97c3c9570eec0319c7
Viswanathan Seetharaman <links96.4u@outlook.com>
2025-04-15 00:01:33 +0530

    Added more additional features to check profiles.

--- config/config.py
+++ config/config.py
@@ -16,8 +16,9 @@ POPUP_WAIT_TIME = 10
 NAVIGATION_WAIT_TIME = 5
 REMINDER_SLEEP_TIME = 2
 MESSAGE_SLEEP_TIME = 4
-PREFERENCE_CHECK_BOX = ['photostatus-visible','recently_joined-1 Week','countryofresidence-India','diet-Non-Veg','diet-Eggetarian','diet-Vegan']
-PREFERENCE_CHECK_BOX_MAX = ['photostatus-visible','recently_joined-1 Week','countryofresidence-India','annualincome-INR 4 Lakh to 7 Lakh','annualincome-INR 7 Lakh to 10 Lakh','annualincome-INR 1 Lakh to 2 Lakh','annualincome-INR 2 Lakh to 4 Lakh','diet-Non-Veg','diet-Eggetarian','diet-Vegan']
+PREFERENCE_CHECK_BOX = ['photostatus-visible','recently_joined-1 Week','diet-Non-Veg','diet-Eggetarian','diet-Vegan']
+PREFERENCE_CHECK_BOX_FOCUS = ['photostatus-visible','search_v3_relevance-1 Week','stateofresidence-Maharashtra','nearest_city-Mumbai','annualincome-INR 4 Lakh to 7 Lakh','annualincome-INR 7 Lakh to 10 Lakh','annualincome-INR 1 Lakh to 2 Lakh','annualincome-INR 2 Lakh to 4 Lakh','diet-Non-Veg','diet-Eggetarian','diet-Vegan']
+PREFERENCE_CHECK_BOX_MORE_FOCUS = ['verified_profiles-verified_profiles','photostatus-visible','search_v3_relevance-1 Week','stateofresidence-Maharashtra','stateofresidence-Madhya Pradesh','annualincome-INR 4 Lakh to 7 Lakh','annualincome-INR 7 Lakh to 10 Lakh','annualincome-INR 1 Lakh to 2 Lakh','annualincome-INR 2 Lakh to 4 Lakh','diet-Non-Veg','diet-Eggetarian','diet-Vegan']
 
 # XPath selectors
 NEXT_BUTTON_XPATH = '//button[contains(text(), "Next")]'
