4f6b0345a972b1aaacba9ccc6977c32999d6fc97
Viswanathan Seetharaman <links96.4u@outlook.com>
2025-04-15 19:29:36 +0530

    Basic template for broadcast message.

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def main():
     print("Choose an option:")
     print("1. Send reminders")
     print("2. Send custom message from file")
-    print("3. Remove inactive profiles")
-    print("4. Open all profiles that are looking for me.")
-    print("5. Open all profiles that in a broader scope.")
-    print("6. Open all profiles that in a premium scope.")
-    print("7. Track online profiles")
+    print("3. Send custom message from file (Broadcast)")
+    print("4. Remove inactive profiles")
+    print("5. Open all profiles that are looking for me.")
+    print("6. Open all profiles that in a broader scope.")
+    print("7. Open all profiles that in a premium scope.")
+    print("8. Track online profiles")
     
     choice = input("Enter your choice (1-6): ")
     
@@ -38,27 +39,32 @@ def main():
             action.process_all_pages()
         elif choice == "3":
             navigate_to_inbox(driver)
-            action = InactiveProfileRemover(driver, By, time, REMINDER_SLEEP_TIME)
+            message = read_message()
+            action = MessageAction(driver, By, time, MESSAGE_SLEEP_TIME, message)
             action.process_all_pages()
         elif choice == "4":
+            navigate_to_inbox(driver)
+            action = InactiveProfileRemover(driver, By, time, REMINDER_SLEEP_TIME)
+            action.process_all_pages()
+        elif choice == "5":
             navigate_to_more_matches(driver)
             navigate_to_see_all(driver, 1)
             check_options_list(driver,1)
             action = MatchesForMe(driver, By, time, NAVIGATION_WAIT_TIME)
             action.process_all_pages()
-        elif choice == "5":
+        elif choice == "6":
             navigate_to_more_matches(driver)
             navigate_to_see_all(driver, 2)
             check_options_list(driver,2)
             action = MatchesForMe(driver, By, time, NAVIGATION_WAIT_TIME)
             action.process_all_pages()
-        elif choice == "6":
+        elif choice == "7":
             navigate_to_more_matches(driver)
             navigate_to_see_all(driver, 3)
             check_options_list(driver,3)
             action = MatchesForMe(driver, By, time, NAVIGATION_WAIT_TIME)
             action.process_all_pages()
-        elif choice == "7":
+        elif choice == "8":
             navigate_to_inbox(driver)
             action = OnlineTracker(driver, By, time, REMINDER_SLEEP_TIME)
             action.process_all_pages()
