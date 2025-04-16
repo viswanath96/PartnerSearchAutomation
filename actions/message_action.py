2034fe6e86c9f168cf1e75b4409cc4d547cce6e6
Viswanathan Seetharaman <links96.4u@outlook.com>
2025-04-16 23:29:50 +0530

    Close any open chats.

--- actions/message_action.py
+++ actions/message_action.py
@@ -17,13 +17,18 @@ class MessageAction(ProfileAction):
                 self.cleanup_tabs()
 
     def process_single_profile(self, link) -> None:
+        # Check if the chat is already opened in a new tab
+        try:
+            self.close_open_chat()
+        except Exception as e:
+            print(f"No open chat to close")
+
         self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", link)
+        
         # Locate the <div> element with title="Chat Now"
         chat_div = self.driver.find_element(self.By.XPATH, CHAT_NOW_XPATH)
-    
         # Check if there is a child element with text "online now"
         child_elements = chat_div.find_elements(self.By.XPATH, ONLINE_NOW_XPATH)
-    
         if not child_elements:
             return
             
@@ -70,4 +75,12 @@ class MessageAction(ProfileAction):
     def cleanup_tabs(self) -> None:
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
+        self.time.sleep(self.sleepTime)
+
+    def close_open_chat(self):
+        close_button = self.driver.find_elements(
+            self.By.XPATH, 
+            '//*[@id="root"]/div/div/div/div[2]/div[3]/div[2]/div[1]/section/div[1]/div/button[2]'
+        )[0]
+        close_button.click()
         self.time.sleep(self.sleepTime)
